60379dcec3d687fa63967e4dee90408dd74a2da2
THEQ <kduck119@gmail.com>
2021-08-17 00:46:46 +0900

    Add Sound

--- Directx12/SoundMgr.cpp
+++ Directx12/SoundMgr.cpp
@@ -79,6 +79,34 @@ void SoundMgr::LoadSoundFile(string FileName, string FilePath)
 
 void SoundMgr::LoadSoundFiles()
 {
+	//bgm
 	LoadSoundFile("nextlevel", "../Resources/Sound/nextlevel.mp3");
 	LoadSoundFile("blackmamba", "../Resources/Sound/blackmamba.mp3");
+
+	//Mouseclick
+	LoadSoundFile("MouseClick", "../Resources/Sound/MouseClick.wav");
+
+	//Att
+	LoadSoundFile("Attack1", "../Resources/Sound/Attack1.wav");
+	LoadSoundFile("Attack2", "../Resources/Sound/Attack2.wav");
+	LoadSoundFile("Attack3", "../Resources/Sound/Attack3.wav");
+	LoadSoundFile("Attack4", "../Resources/Sound/Attack4.wav");
+	LoadSoundFile("Attack5", "../Resources/Sound/Attack5.wav");
+
+	//Walk
+	LoadSoundFile("Walk1", "../Resources/Sound/Walk1.wav");
+	LoadSoundFile("Walk2", "../Resources/Sound/Walk2.wav");
+
+	//Skill
+	LoadSoundFile("FireWall", "../Resources/Sound/FireWall.mp3");
+	LoadSoundFile("Meteor", "../Resources/Sound/Meteor.mp3");
+	LoadSoundFile("Boom", "../Resources/Sound/Boom.wav");
+
+	LoadSoundFile("IceField", "../Resources/Sound/IceField.mp3");
+	LoadSoundFile("IceFreezeMake", "../Resources/Sound/IceFreezeMake.wav");
+	LoadSoundFile("IceFreezeDestroy", "../Resources/Sound/IceFreezeDestroy.wav");
+	LoadSoundFile("Crevas", "../Resources/Sound/Crevas.wav");
+
+	LoadSoundFile("BlackHole", "../Resources/Sound/BlackHole.wav");
+	LoadSoundFile("Hatred", "../Resources/Sound/Hatred.wav");
 }
